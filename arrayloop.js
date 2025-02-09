c2e45f7febadd71952da7da2d05912da950df39d
inelika <dosanenlik@gmail.com>
2025-02-09 20:40:37 +0500

    Update arrayloop.js

--- arrayloop.js
+++ arrayloop.js
@@ -1,4 +1,4 @@
 const array =["nastya", "asem","ardana", "yenlik" ,"liza","dayana","aqtolqun", "nika"]
-for ( let i = 0; i<=7; i++){
-console.log(array[i])
+for ( let i = 0; i<array.length; i++){
+console.log(array[i]);
 }
